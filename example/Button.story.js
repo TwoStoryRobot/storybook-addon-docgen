58832bbe326b4bcee7773c92a651055cfcdc2f43
Chad Fawcett <me@chadf.ca>
2018-01-25 18:44:06 -0800

    Add docs example to Button story

--- example/Button.story.js
+++ example/Button.story.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
+import docgen from '../src/index'
 
 import Button from './Button'
 
 storiesOf('Button', module)
+  .addDecorator(docgen(Button))
   .add('with text', () => (
     <Button onClick={action('clicked')}>Hello Button</Button>
   ))
